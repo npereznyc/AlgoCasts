110179e79c89583ad3a13dbe3ff32942845315dd
npereznyc <natalieperez84@gmail.com>
2023-02-10 11:13:00 -0500

    added alternate solution to problem 2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,18 +7,40 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//     //reverse string - save as new string while maintaning the old string
+//     //compare new string to old
+//     let reversed=""
+//     for (let i = str.length-1; i >= 0; i--) {
+//         reversed += str[i]
+//     }
+//     if (reversed === str){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+//more condensed way to run that if statement:
+
+// function palindrome(str) {
+//     //reverse string - save as new string while maintaning the old string
+//     //compare new string to old
+//     let reversed=""
+//     for (let i = str.length-1; i >= 0; i--) {
+//         reversed += str[i]
+//     }
+//     return str === reversed
+// }
+// palindrome("abba")
+// palindrome("pennep")
+
 function palindrome(str) {
-    //reverse string - save as new string while maintaning the old string
-    //compare new string to old
-    let reversed=""
-    for (let i = str.length-1; i >= 0; i--) {
-        reversed += str[i]
-    }
-    if (reversed === str){
-        return true
-    } else {
-        return false
-    }
+    //convert string to array
+    //use .every to check if elements within the array match each other - this is not an ideal solution because you do twice as much work
+    return str.split('').every((char, i)=> {
+        return char === str[str.length-i-1]
+    })
 }
 palindrome("abba")
 palindrome("pennep")
