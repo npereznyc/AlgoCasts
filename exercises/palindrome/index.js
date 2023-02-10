d047f5113afafc40243e9facd533994181c27e06
npereznyc <natalieperez84@gmail.com>
2023-02-10 11:03:50 -0500

    solved problem 2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,20 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    //reverse string - save as new string while maintaning the old string
+    //compare new string to old
+    let reversed=""
+    for (let i = str.length-1; i >= 0; i--) {
+        reversed += str[i]
+    }
+    if (reversed === str){
+        return true
+    } else {
+        return false
+    }
+}
+palindrome("abba")
+palindrome("pennep")
 
 module.exports = palindrome;
