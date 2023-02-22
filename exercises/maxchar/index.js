d8244ff8cee12fdc849261318cb46d03dd94efd4
npereznyc <natalieperez84@gmail.com>
2023-02-22 16:20:39 -0600

    working on maxchar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    //iterate over string
+    //add characters to new object
+    //assign count to each character and increment it by 1 every time it's passed over
+    const chars = {}
+    for (let char of str) {
+        chars[char] = chars[char]+1 || 1
+    }
+    console.log(chars)
+    const keys = Object.keys(chars)
+    console.log('value1: ', keys[0])
+ }
 
 module.exports = maxChar;
+
+maxChar("abcccccccd") === "c"
