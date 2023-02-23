73f8c02ad806c1b25e1975405b89f589ed4a53ff
npereznyc <natalieperez84@gmail.com>
2023-02-23 07:53:26 -0600

    getting closer on maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,17 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+//IN PROGRESS. NOT WORKING YET. HAVE NOT LOOKED AT SOLUTION
+
 function maxChar(str) {
     //iterate over string
     //add characters to new object
     //assign count to each character and increment it by 1 every time it's passed over
+
+    //instantiate empty object
     const chars = {}
+    //iterate over string, assigning count to each character and increment it by 1 every time it's passed over
     for (let char of str) {
         chars[char] = chars[char]+1 || 1
     }
-    console.log(chars)
-    const keys = Object.keys(chars)
-    console.log('value1: ', keys[0])
+    console.log(chars) // { a: 1, b: 1, c: 7, d: 1 }
+    const values = Object.values(chars)
+    console.log('value1: ', values[0]) // value1:  1
  }
 
 module.exports = maxChar;
