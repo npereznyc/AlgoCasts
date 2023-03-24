739f4ea9c8d2e035d4bf19437d813a060501fd2b
npereznyc <natalieperez84@gmail.com>
2023-03-24 10:03:39 -0400

    solved fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,30 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//instantiate an array with 0 and 1
+//iterate from 0 to n
+//push the sum of the last two elements into the array
+//return the last element of the array
 
+//write a function that uses recursion to return the nth element of the fibonacci sequence
+
+function fib(n) {
+    if (n < 2) {
+        return n
+    }
+    return fib(n-1) + fib(n-2)
+}
+fib(15) 
 module.exports = fib;
+
+
+// function fib(n) {
+//     let fibarr = [0,1]
+//     for (let i = 2; i <= n; i++) {
+//         fibarr.push(fibarr[i-2] + fibarr[i-1])
+//     }
+//     // console.log(fibarr)
+//     return fibarr[n]
+// }
+// fib(15) 
+// module.exports = fib;
