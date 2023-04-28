bd89c3d691973698a31b28f9bda7027f7da06d84
npereznyc <natalieperez84@gmail.com>
2023-04-28 11:27:26 -0400

    continuing to work on linked lists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -32,15 +32,65 @@ class LinkedList {
         }
         return counter
     }
+
+    getFirst() {
+        return this.head
+    }
+
+    getLast(){
+        //accounting for the scenario where there are no nodes in the list:
+        if(!this.head){ 
+            return null
+        } 
+
+        let node = this.head //reference to the first node in the list
+        // console.log(node)
+        while(node){
+            if(!node.next){
+                return node; //if node.next is null, return the node because it is the tail
+            }
+            node=node.next; //otherwise, update the node to the node.next value
+            }
+    }
+
+    clear(){
+        this.head=null; //the linked list only knows that there are nodes because it's head has a value. If we make the head null, then there is effectively no list.
+    }
+
+    removeFirst(){
+        if(!this.head){
+            return
+        }
+        this.head=this.head.next //we want the head to point to the second node in the list, effectively removing the first node. If there is no second node, then we're reassigning this.head to null, which is also what we're looking for.
+    }
+
+    removeLast(){
+        if(!this.head){
+            return;
+        }
+        if(!this.head.next){
+            this.head=null
+            return;
+        }
+
+        let previous=this.head //assigning the head to the varaible previous
+        let current=this.head.next //assigning the net node to the value current
+
+        while(current.next){ //this loop will run as long as current.next is not null
+            previous=current //previous is reassinged to the current node
+            current=current.next //the current node is reassigned to the next node
+        }
+        previous.next=null //when the while loop is done running, we will have reached the end of the list, so we assign previous.next to null, which eliminates the current node(the last node)
+    }
  }
 
 //real usage of linked list:
-const list = new LinkedList('a');
-// list.head = new Node('a')
+const list = new LinkedList();
+list.head = new Node('a')
 list.insertFirst('b');
 list.insertFirst('c');
-list.insertFirst('d');
-console.log(list.size())
+// list.insertFirst('d');
+console.log(list.removeLast())
 
 // const list = new LinkedList(10)
     //this would create a new linked list with a head node that has 10 as the data value; next would be null.
